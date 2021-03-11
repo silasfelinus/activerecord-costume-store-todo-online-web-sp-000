f1b187a5fd5c0fef7044be904518c3eae163b102
Silas Michael Knight <silasfelinus@gmail.com>
2021-03-11 02:26:38 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -9,6 +9,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.boolean :family_friendly
       t.datetime :opening_date
       t.datetime :closing_date
+      t.string :description
       t.timestamps
     end
   end
